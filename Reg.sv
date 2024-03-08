e530b51254dddd9edc884d9e0d6f47a747ce48a1
Đắc Tâm <136346435+Stork1323@users.noreply.github.com>
2024-03-08 15:44:35 +0700

    Update Reg.sv

--- Reg.sv
+++ Reg.sv
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 module Reg( // Register File
 	input logic [31:0] dataW_i,
 	input logic [4:0] rsW_i, rs1_i, rs2_i,
@@ -26,33 +26,5 @@ module Reg( // Register File
 	
 endmodule
 
-=======
-module Reg( // Register File
-	input logic [31:0] dataW_i,
-	input logic [4:0] rsW_i, rs1_i, rs2_i,
-	input logic RegWEn_i,
-	input logic clk_i,
-	output logic [31:0] data1_o, data2_o
-	);
-	
-	logic [31:0] Regs [0:4]; // 32 bit register
-	assign Regs[0] = 32'b0;
-	
-	// Write data on register
-	logic [31:0] temp;
-	mux2to1_32bit M0(Regs[rsW_i], dataW_i, RegWEn_i, temp);
-	
-	
-	
-	always_ff @(posedge clk_i) begin
-		Regs[rsW_i] <= temp;
-	end
-	
-	// Update data output
-	assign data1_o = Regs[rs1_i];
-	assign data2_o = Regs[rs2_i];
-	
-endmodule
 
->>>>>>> 7f3121a0d51f8338c8c121c360899e1d92163c75
 	
